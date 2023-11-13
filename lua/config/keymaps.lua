068a01b4bf243fffea683f1dc8c352c86f50c0b8
= <=>
2023-11-13 19:53:20 -0300

    Adicionado mais pacotes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,22 +7,22 @@ mapkey("<leader>bb", "e #", "n") -- Switch to Other Buffer
 mapkey("<leader>`", "e #", "n") -- Switch to Other Buffer
 
 -- Directory Navigatio}n
-mapkey("<leader><Left>", "NvimTreeFocus", "n")
-mapkey("<leader><Tab>", "NvimTreeToggle", "n")
+mapkey("<leader>m", "NvimTreeFocus", "n")
+mapkey("<leader>e", "NvimTreeToggle", "n")
 
 -- Pane and Window Navigation
 mapkey("<C-h>", "<C-w>h", "n") -- Navigate Left
 mapkey("<C-j>", "<C-w>j", "n") -- Navigate Down
 mapkey("<C-k>", "<C-w>k", "n") -- Navigate Up
 mapkey("<C-l>", "<C-w>l", "n") -- Navigate Right
---mapkey("<C-h>", "wincmd h", "t") -- Navigate Left
---mapkey("<C-j>", "wincmd j", "t") -- Navigate Down
---mapkey("<C-k>", "wincmd k", "t") -- Navigate Up
---mapkey("<C-l>", "wincmd l", "t") -- Navigate Right
---mapkey("<C-h>", "TmuxNavigateLeft", "n") -- Navigate Left
---mapkey("<C-j>", "TmuxNavigateDown", "n") -- Navigate Down
---mapkey("<C-k>", "TmuxNavigateUp", "n") -- Navigate Up
---mapkey("<C-l>", "TmuxNavigateRight", "n") -- Navigate Right
+mapkey("<C-h>", "wincmd h", "t") -- Navigate Left
+mapkey("<C-j>", "wincmd j", "t") -- Navigate Down
+mapkey("<C-k>", "wincmd k", "t") -- Navigate Up
+mapkey("<C-l>", "wincmd l", "t") -- Navigate Right
+mapkey("<C-h>", "TmuxNavigateLeft", "n") -- Navigate Left
+mapkey("<C-j>", "TmuxNavigateDown", "n") -- Navigate Down
+mapkey("<C-k>", "TmuxNavigateUp", "n") -- Navigate Up
+mapkey("<C-l>", "TmuxNavigateRight", "n") -- Navigate Right
 
 -- Window Management
 mapkey("<leader>sv", "vsplit", "n") -- Split Vertically
